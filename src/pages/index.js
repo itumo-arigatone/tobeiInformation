52857afa4ed608f56cbd9ed452e210137951c517
unkomorasi01 <soganokasegi0825@gmail.com>
2020-11-29 18:49:59 +0900

    ようやく理解したわ

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
+import Head from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Head headerText="Check-out"/>
+      <div>
+          <strong>
+              10:00a.m.
+          </strong>
+      </div>
+      <br/>
+      <span>&nbsp;※ After checking out we will keep baggage until departure.</span>
+    </div>
+    );
 }
