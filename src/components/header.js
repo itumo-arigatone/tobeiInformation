7838c9cf0962a05b5c7243ebe76a96fd1d4ec9a7
unkomorasi01 <soganokasegi0825@gmail.com>
2020-11-30 23:53:39 +0900

    スタイルを追加

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,33 @@
 import React from "react"
 
-export default props => <h1>{props.headerText}</h1>
+export const Head = props => <h1>{props.headerText}</h1>
+
+export const ImportantTime = props =>
+    <div className="importantTime">
+        <strong>
+            {props.time}
+        </strong>
+    </div>
+
+export const OtherTime = props =>{
+    if (props.option == "undefined" || props.option == null) {
+        return(
+            <div className="otherTime">
+                <strong>
+                    &nbsp;{props.time}
+                </strong>
+            </div>
+        );
+    }
+    return (
+        <div className="otherTime">
+            &nbsp;{props.option}
+            <br/>
+            <strong>
+                &nbsp;{props.time}
+            </strong>
+        </div>
+    );
+}
+
+export const Comment = props => <span>&nbsp; {props.comment}</span>
