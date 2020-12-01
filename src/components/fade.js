15e2b98eba81d92e225b7f211d2e700e81a2a668
satou <satou@logos.co.jp>
2020-12-01 13:12:23 +0900

    add image

--- src/components/fade.js
+++ src/components/fade.js
@@ -1,4 +1,6 @@
 import React from "react"
+import Img from "../images/information.png"
+import "../styles/first.scss"
 
 export default class Fade  extends React.Component {
     state = {
@@ -6,23 +8,28 @@ export default class Fade  extends React.Component {
         isHide: false,
     };
     timeout = setTimeout(() => { this.setState({isOpen: false}); console.log("ちめ"); }, 3000);
-    timeout = setTimeout(() => { this.setState({isHile: true}); console.log("ちめ"); }, 4000);
+    timeout = setTimeout(() => { this.setState({isHide: true}); console.log("ちめ"); }, 4000);
 
     render() {
         return (
-            <div className="farst"
+            <div className="first"
             style={{
                 position: "absolute",
                 transition: '1s',
                 opacity: this.state.isOpen ? 1 : 0,
-                width: "100%",
+                width: "95%",
                 height: "100%",
                 display: this.state.isHide ? "none" : "",
             }}
             >
-                <span className="farst-message">
-                    Information
-                </span>
+                <div className="category">
+                    <div className="first-message">
+                        Tobei
+                        <br/>
+                        Information
+                    </div>
+                    <img src={ Img } className="first-image" />
+                </div>
           </div>
         );
     };
